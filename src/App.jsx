24f5a5961125c4843eb5aceaacdbdaefca91dcad
emir paradi <emir.alejandro.paradi@gmail.com>
2024-01-29 20:34:50 -0300

    Nav y footer agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Container } from "react-bootstrap";
 import FormularioPeliculas from "./components/FormularioPeliculas";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Container className="main my-5">
-      <h1 className="text-secondary mb-5 text-center">
-        Administrador de Películas
-      </h1>
-      <FormularioPeliculas></FormularioPeliculas>
-    </Container>
+    <>
+      <Navbar></Navbar>
+      <Container className="main my-5">
+        <h1 className="text-secondary mb-5 text-center">
+          Administrador de Películas
+        </h1>
+        <FormularioPeliculas></FormularioPeliculas>
+      </Container>
+      <Footer></Footer>
+    </>
   );
 }
 
